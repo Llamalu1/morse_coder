e48cb8da3010a0bb5cdef02caee27e0ad0ca3611
Llamalu1 <Llamalu1@gmail.com>
2025-09-03 16:31:16 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+morse_code = {
+    # Letters (A-Z)
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+
+    # Digits (0-9)
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+
+    # Punctuation/Symbols (optional)
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--',
+    '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...', ':': '---...',
+    ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-',
+    '"': '.-..-.', '$': '...-..-', '@': '.--.-.', ' ': '/'
+}
+
+user_input = (input('Please enter a word or sentence:\n')).upper()
+
+result =[]
+for char in user_input:
+    if char in morse_code:
+        result.append(morse_code[char])  # Append  letter Morse equivalent
+    else:
+        result.append('Character not supported')
+
+# Join Morse Code with spaces between characters
+morse_output = ' '.join(result)
+print(morse_output)
